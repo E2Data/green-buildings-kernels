06cf914d33dfc9aae403be7eb6cc55acca963eba
Nikos Kanakis <full290@gmail.com>
2019-08-12 08:51:29 +0300

    tornado cleanup

--- src/main/java/net/sparkworks/e2data/AnalyticsSampleEngine.java
+++ src/main/java/net/sparkworks/e2data/AnalyticsSampleEngine.java
@@ -30,54 +30,8 @@ public class AnalyticsSampleEngine {
     }
     
     private static void executeAnalytics(final String arg, final double[] samples) {
-//        final AnalyticsProcessor analyticsProcessor = AnalyticsProcessor.getInstance();
         final double[] result = new double[1];
-        TaskSchedule task0 = new TaskSchedule("s0")
-                .streamIn(samples)
-                .task("t0", AnalyticsProcessor::computeMin, samples, result)
-                .streamOut(result);
-        ExecutionTime.printTime(() -> task0.execute());
-        System.out
-                .println(String.format(" computing Min of %s random samples with result %f", arg, result[0]));
-    
-        TaskSchedule task1 = new TaskSchedule("s1")
-                .streamIn(samples)
-                .task("t1", AnalyticsProcessor::computeMax, samples, result)
-                .streamOut(result);
-        ExecutionTime.printTime(() -> task1.execute());
-        System.out
-                .println(String.format(" computing Max of %s random samples with result %f", arg, result[0]));
-        
-        TaskSchedule task2 = new TaskSchedule("s2")
-                .streamIn(samples)
-                .task("t2", AnalyticsProcessor::computeSum, samples, result)
-                .streamOut(result);
-        ExecutionTime.printTime(() -> task2.execute());
-        System.out
-                .println(String.format(" computing Sum of %s random samples with result %f", arg, result[0]));
-    
-        TaskSchedule task3 = new TaskSchedule("s3")
-                .streamIn(samples)
-                .task("t3", AnalyticsProcessor::computeAvg, samples, result)
-                .streamOut(result);
-        ExecutionTime.printTime(() -> task3.execute());
-        System.out
-                .println(String.format(" computing Avg of %s random samples with result %f", arg, result[0]));
-    
-        final double[] taskOutliersResult = new double[3];
-        TaskSchedule task4 = new TaskSchedule("s4")
-                .streamIn(samples)
-                .task("t4", AnalyticsProcessor::removeOutliers, samples, taskOutliersResult)
-                .streamOut(result);
-        ExecutionTime.printTime(() -> task4.execute());
-        System.out
-                .println(String
-                        .format(" computing Outliers of %s random samples with mean %f, standard deviation %f and outliers count %f",
-                                arg, taskOutliersResult[0], taskOutliersResult[1], taskOutliersResult[2]));
-    
-        
         // vanilla
-/*
         ExecutionTime.printTime(() -> AnalyticsProcessor.computeMin(samples, result));
         System.out
                 .println(String.format(" computing Min of %s random samples with result %f", arg, result[0]));
@@ -98,7 +52,6 @@ public class AnalyticsSampleEngine {
         ExecutionTime.printTime(() -> AnalyticsProcessor.removeOutliers(samples, outliersResult));
         System.out
                 .println(String.format(" computing Outliers of %s random samples with mean %f, standard deviation %f and outliers count %f", arg, outliersResult[0], outliersResult[1], outliersResult[2]));
-*/
     }
     
     private static boolean isNumeric(final String arg) {
